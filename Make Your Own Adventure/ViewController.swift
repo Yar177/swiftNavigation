f85660484e328fc731ffc421018ff3b33a7b20be
Yar Sher <yarser.dev@gmail.com>
2019-02-25 16:05:19 -0500

    Initial Commit

---
+++ Make Your Own Adventure/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Make Your Own Adventure
+//
+//  Created by Yar Sher on 2/25/19.
+//  Copyright © 2019 Yar Sher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Make Your Own AdventureTests/Make_Your_Own_AdventureTests.swift
@@ -0,0 +1,34 @@
+//
+//  Make_Your_Own_AdventureTests.swift
+//  Make Your Own AdventureTests
+//
+//  Created by Yar Sher on 2/25/19.
+//  Copyright © 2019 Yar Sher. All rights reserved.
+//
+
+import XCTest
+@testable import Make_Your_Own_Adventure
+
+class Make_Your_Own_AdventureTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
